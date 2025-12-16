1b317d50cbecac49f8ec075dcd70c9656eced937
Nivetha-26 <nivethasri0641@gmail.com>
2025-12-16 07:55:04 +0530

    pro

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,7 +30,7 @@ function Header() {
         <a href="#about" onClick={handleCloseMenu}>About</a>
         <a href="#award" onClick={handleCloseMenu}>Awards & Certifications</a>
         <a href="#skill" onClick={handleCloseMenu}>Tools & Skills</a>
-        <a href="#contact" onClick={handleCloseMenu}>Contact</a>
+        <a href="#project" onClick={handleCloseMenu}>Projects</a>
         <a href="/resume.pdf" download className="btn-resume" onClick={handleCloseMenu}>Download Resume</a>
         <a href="#contact" className="btn-talk" onClick={handleCloseMenu}>Let's Talk</a>
       </nav>
